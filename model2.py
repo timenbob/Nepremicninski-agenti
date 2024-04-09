e87950d2dac0f09bb4bf6cff631cac9a3dec047e
timenbob <timen.bobnar@gmail.com>
2024-04-09 12:30:14 +0200

    k

--- model2.py
+++ model2.py
@@ -32,12 +32,16 @@ class Agenti:
     Razred za agente.
     """
 
-    def __init__(self, ime, *, id=None):
+    def __init__(self, ime, kontakt,geslo,naziv, id=None):
         """
         Konstruktor uporabnika.
         """
         self.id = id
         self.ime = ime
+        self.kontakt=kontakt
+        self.geslo=geslo
+        self.naziv=naziv
+
 
     def __str__(self):
         """
@@ -47,3 +51,31 @@ class Agenti:
         return self.ime
 
 
+class Kupci:
+    def __init__ (self,ime,kontakt,buget,lokacija,vrsta, id):
+        self.id=id
+        self.ime = ime
+        self.kontakt=kontakt
+        self.buget=buget
+        self.lokacija=lokacija
+        self.vrsta=vrsta
+
+
+class Nepremicnine:
+    def __init__ (self,lastnik,cena,lokacija,vrsta, id):
+        self.id=id
+        self.lastnik = lastnik
+        self.cena=cena
+        self.vrsta=vrsta
+        self.lokacija=lokacija
+        
+
+class Zastopa:
+    def __init__(self,kupec,agent):
+        self.kupec=kupec
+        self.agent=agent
+
+class Interes:
+    def __init__(self,kupec,nepremicnina):
+        self.kupec=kupec
+        self.nepremicnina=nepremicnina
